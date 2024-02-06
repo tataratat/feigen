195adb7145bfe92b554c78e176b756555ef1e2f1
Jaewook Lee <jaewooklee042@gmail.com>
2024-02-06 07:36:03 +0100

    configurable weights min/max

--- feigen/nurbs_weights.py
+++ feigen/nurbs_weights.py
@@ -92,7 +92,7 @@ class NURBSWeights(vedo.Plotter, FeigenBase):
 
     __slots__ = ("_c", "_s")
 
-    def __init__(self, spline=None):  # noqa PLR0915
+    def __init__(self, spline=None, w_min=_W_MIN, w_max=_W_MAX):  # noqa PLR0915
         """
         Create spline and setup callbacks
         """
@@ -118,6 +118,10 @@ class NURBSWeights(vedo.Plotter, FeigenBase):
 
         # set title name
         self._c["window_title"] = "NURBS Weights"
+
+        # set weights extrema
+        self._c["w_min"] = w_min
+        self._c["w_max"] = w_max
 
         # sampling resolutions
         default_sampling_resolution = 50
@@ -360,8 +364,8 @@ class NURBSWeights(vedo.Plotter, FeigenBase):
             self._weight_slider,
             pos1=[*pos1, 0],
             pos2=[*pos2, 0],
-            xmin=_W_MIN,
-            xmax=_W_MAX,
+            xmin=self._c["w_min"],
+            xmax=self._c["w_max"],
             value=1,
             s=0.04,
             title="weight",
